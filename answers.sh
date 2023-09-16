3405ac4699d480e0ff865d53f688c19a8a15bb66
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-09-16 22:04:56 +0300

    Joins

--- answers.sh
+++ answers.sh
@@ -21,3 +21,16 @@ B . Creating Tables:
 11. INSERT INTO graduates ( name , Age , Gender , Points , Graduation_Date )
     VALUES ("Layal" , 18 , "F" , 350 , "08/09/2018" )
 12. DELETE FROM students WHERE name = "Layal"
+
+
+C. Joins 
+14. SELECT employees.Name , companies.Name , companies.Date 
+    FROM employees 
+    INNER JOIN companies ON 
+    employees.Company = companies.name
+15. SELECT employees.name FROM employees 
+    LEFT JOIN companies on employees.Company = companies.name 
+    WHERE companies.Date < '2000'
+16. SELECT companies.Name FROM companies
+    LEFT JOIN employees ON companies.name = employees.Company
+    WHERE employees.Role = 'Graphic Designer'
