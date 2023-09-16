f14e75583daac8e890296426c22037a25665c109
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-09-16 19:41:50 +0300

    Finished the basec queries

---
+++ answers.sh
@@ -0,0 +1,9 @@
+1. SELECT name FROM students 
+2. SELECT * FROM students WHERE Age > 30 
+3. SELECT name FROM students WHERE Age > 30 AND Gender = "F"
+4. SELECT Points FROM students WHERE Name = "Alex" 
+6. INSERT INTO students ( Name , age , gender , points) 
+   VALUES ("WOUROUD" , 19 , "F" , 1098)  
+7. UPDATE students SET Points = Points + 1 WHERE name = "Basma" ;
+   UPDATE students SET Points = Points - 1 WHERE name = "Alex" ;
+
